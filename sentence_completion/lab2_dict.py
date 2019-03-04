7d540ab541cfbd592e788b6e2810abffca43c1e8
nyxflower <hxu31@sheffield.ac.uk>
2019-03-04 01:46:55 +0000

    finished

--- sentence_completion/lab2_dict.py
+++ sentence_completion/lab2_dict.py
@@ -10,8 +10,10 @@ The data used in this code is from the following link.
 """
 from collections import Counter
 import numpy as np
+import os
 import re
 import time
+import sys
 
 N_ROW, N_COLUMN = 2, 10
 D_TYPE_INT = np.dtype('uint8')
@@ -21,14 +23,9 @@ D_TYPE_FLOAT = np.dtype('float64')
 #                   RUN FROM HERE                  #
 # ################################################ #
 if __name__ == '__main__':
-    # abs_path = os.path.abspath('.')
-    # corpus_path = os.path.join(abs_path, sys.argv[1])
-    # question_path = os.path.join(abs_path, sys.argv[2])
-
-    corpus_path = '/Users/nyxfer/Documents/GitHub/nlp/sentence_completion/news-corpus-500k.txt'
-    question_path = '/Users/nyxfer/Documents/GitHub/nlp/sentence_completion/questions.txt'
-
-    # corpus = "Colorado's six-run through threw went know weather through whether whether fourth , however , combined the sort of bad luck and poor play that dogged the Diamondbacks .Sport wagons combine nearly all the velocity and road-grabbing craftiness of a proper sport sedan with the family-friendly utility of a SUV / crossover .By the way - the first amendment is words also - Words to live by , words that guide , explain , and give meaning and purpose .Nothing would boost consumer confidence and banker confidence more than a consensus that property prices have really bottomed out .Instead , offers on fresh food have risen.We don't provide detail on ongoing operations".lower()
+    abs_path = os.path.abspath('.')
+    corpus_path = os.path.join(abs_path, sys.argv[1])
+    question_path = os.path.join(abs_path, sys.argv[2])
 
     # read from corpus to a string (line by line)
     t = time.time()
@@ -39,10 +36,6 @@ if __name__ == '__main__':
         corpus_list.append("<s>")
         corpus_list.extend(re.sub("[^\w]", ' ', line.lower()).split())     # convert to lower case
         corpus_list.append('</s>')
-    print(time.time() - t)
-
-
-    # corpus_list = re.sub("[^\w]", " ", corpus).split()
 
     # counter for Unigram and Bigram
     counter_unigram = Counter(corpus_list)
@@ -65,33 +58,18 @@ if __name__ == '__main__':
         frequency_2d[r, c] = value
 
     # read from question file to three matrix
-    # t = time.time()
     with open(question_path, 'r') as q:
         question_list = q.readlines()
-    # corpus_list = []
-    # for line in s:  # add <s> and <\s> at the begin and end of each line
-    #     corpus_list.append("<s>")
-    #     corpus_list.extend(re.sub("[^\w]", '', line.lower()).split())  # convert to lower case
-    #     corpus_list.append('</s>')
-    # print(time.time() - t)
-
-    # question_list = ["I don't know ____ to go out or not . : weather/whether",
-    #                  "We went ____ the door to get inside . : through/threw"]
     sub_pattern = re.compile(r'____')          # when print sub it to answer
 
-    # question_sp_list = [question.split() for question in question_list]
-
-    # candidate_word_list = [re.split("\/", question_sp[-1]) for question_sp in question_sp_list] # list of candidate list
     index = 0
 
     c1_list = []
     c2_list = []
-
     join_c1 = np.zeros(shape=(N_ROW, N_COLUMN), dtype=D_TYPE_FLOAT)
     join_c2 = np.zeros(shape=(N_ROW, N_COLUMN), dtype=D_TYPE_FLOAT)
     marg_c1 = []
     marg_c2 = []
-    # marg_pre = []
 
     for question in question_list:
         sp = question.split()
@@ -112,49 +90,25 @@ if __name__ == '__main__':
         join_c1[:, index] = [frequency_2d[pre, coo_c1], frequency_2d[coo_c1, ne]]
         join_c2[:, index] = [frequency_2d[pre, coo_c2], frequency_2d[coo_c2, ne]]
 
-        # marg_pre.append(frequency_2d[pre, pre])
-
         index += 1
 
-        # pre = coordinate_dict[sp[blank_index - 1]]      # coordinate of previous word
-        # ne = coordinate_dict[sp[blank_index + 1]]       # coordinate of next word
-        # fre_pre_c1, fre_pre_c2 = frequency_2d[pre, coo_c1], frequency_2d[pre, coo_c2]
-        # fre_c1_next, fre_c2_next = frequency_2d[coo_c1, ne], frequency_2d[coo_c2, ne]
-        # pre = frequency_2d[pre, pre]                    # frequency of previous word
-        #
-        # score_c1 =  math.log()
-
-    # print()
     answer_uni = np.where(marg_c1 > marg_c2, c1_list, c2_list)
-
 
     # bigram
     score_c1 = np.sum(np.log(join_c1 + 0.0001), axis=0) - np.log(marg_c1)
     score_c2 = np.sum(np.log(join_c2 + 0.0001), axis=0) - np.log(marg_c2)
     answer_bi = np.where(score_c1 > score_c2, c1_list, c2_list)
-    # print()
 
     # bigram with smoothing
     score_c1 = np.sum(np.log(join_c1 + 1), axis=0) - np.log(np.array(marg_c1) + dimension)
     score_c2 = np.sum(np.log(join_c2 + 1), axis=0) - np.log(np.array(marg_c2) + dimension)
     answers_sm = np.where(score_c1 > score_c2, c1_list, c2_list)
 
-    # for i in range(N_COLUMN):
-    #     print('Question ', i+1, " :")
-    #     print(' ', question_list[i])
-    #     print("  -> Unigram: ", answer_uni[i],
-    #           "  -> Bigram: ", answer_bi[i],
-    #           "  -> Bigram with Smoothing: ", answers_sm[i])
-    #
-    # print(time.time() - t)
+    for i in range(N_COLUMN):
+        print('Question ', i+1, " :")
+        print(' ', question_list[i])
+        print("  -> Unigram: ", answer_uni[i],
+              "  -> Bigram: ", answer_bi[i],
+              "  -> Bigram with Smoothing: ", answers_sm[i])
 
-    answer = ['whether', 'through', 'piece', 'court', 'allowed',
-              'check', 'hear', 'cereal', 'chews', 'sell']
-    score_compare = ['<', '', '', '', '',
-                     '', '', '', '', '']
-
-# def compute_score(score_c1_list, score_c2_list):
-#     diff = score_c1_list - score_c2_list
-#     score = 0.
-#     for d in diff:
-#
+    print(time.time() - t)
--- sentence_completion/lab2_sparse.py
+++ sentence_completion/lab2_sparse.py
@@ -25,56 +25,22 @@ class SentenceComplete:
         print("size of 157594*157594 'uint8' lil_matrix:     %5d bytes " % sys.getsizeof(lil_matrix))
 
     def __construct_count_matrix(self, corpus):
-        # counter_unigram = Counter(corpus)
-        # dimension = len(counter_unigram)
+        values = np.ones(self.len_corpus, dtype=np.dtype('uint8'))
 
-        # construct coordinate dictionary
-        # keys = list(counter_unigram.keys())
-        # values = list(i for i in range(dimension))
-
-        # sparse matrix for unigram: diagonal
-        # coo_uni = coo_matrix((list(counter_unigram.values()),
-        #                       (values, values)),
-        #                      shape=(dimension, dimension),
-        #                      dtype=np.dtype('uint8'))
-
-        # construct bigram counter (list of string pairs -> list of int pairs)
-        # int_corpus = [self.coordinate_dict[word] for word in corpus]
-        # counter_bigram = Counter(zip(int_corpus, int_corpus[1:]))
-
-        # sparse matrix for bigram
-        # rc = np.array(list(counter_bigram.keys()), dtype=str)
-        # r = [self.coordinate_dict[a] for a in rc[:, 0]]
-        # c = [self.coordinate_dict[a] for a in rc[:, 1]]
-
-        # coo_bi = coo_matrix((list(counter_bigram.values()),
-        #                      (r, c)),
-        #                     shape=(dimension, dimension),
-        #                     dtype=np.dtype('uint8'))
-
-        # lil_bi = coo_matrix((dimension, dimension), dtype=np.dtype('uint8')).tolil()
-        # for key, value in counter_bigram.items():
-        #     lil_bi[key] = value
-
-        # ----------------------------------------------------------#
-        values = [1 for i in range(self.len_corpus)]
         # sparse matrix for unigram: diagonal
         coo_uni = coo_matrix((values, (corpus, corpus)),
                              shape=(self.len_corpus, self.len_corpus),
                              dtype=np.dtype('uint8'))
 
         # sparse matrix for bigram
-        values.pop()
-        coo_bi = coo_matrix((values, (corpus[:-1], corpus[1:])),
+        coo_bi = coo_matrix((values[:-1], (corpus[:-1], corpus[1:])),
                             shape=(self.len_corpus, self.len_corpus),
                             dtype=np.dtype('uint8'))
 
-        # ----------------------------------------------------------#
         coo = coo_uni + coo_bi
         print("size of 157594*157594 'uint8' coo_matrix : %5d bytes" % sys.getsizeof(coo))
 
         return coo.tolil()
-        # return coo_uni.tolil() + lil_bi
 
     def test(self, questions, num_q=10):
         count_c1 = np.zeros(shape=(1, num_q), dtype=np.dtype('uint8'))
@@ -102,37 +68,32 @@ class SentenceComplete:
             count_join_c1[:, i] = [self.count_lil[coo_pre, coo_c1], self.count_lil[coo_c1, coo_next]]
             count_join_c2[:, i] = [self.count_lil[coo_pre, coo_c2], self.count_lil[coo_c2, coo_next]]
 
+        print()
         print("Join count: first row for candidate with the previous, second for the next")
         print("--> c1: ")
         print(count_join_c1)
         print("--> c2: ")
         print(count_join_c2)
 
+        print()
         # Unigram
         print("Unigram: ")
-        print("--> The first candidate:  ", ', '.join('{:6d}'.format(f) for f in count_c1.flatten()))
-        print("--> The second candidate: ", ', '.join('{:6d}'.format(f) for f in count_c2.flatten()))
+        print("--> The first candidate:  ", ', '.join('{:6d}'.format(k) for k in count_c1.flatten()))
+        print("--> The second candidate: ", ', '.join('{:6d}'.format(p) for p in count_c2.flatten()))
 
         # Bigram
         print("Bigram: ")
         score = count_join_c1[0, :] * count_join_c1[1, :] / count_c1
-        print("--> The first candidate:  ", ', '.join('{:6.2f}'.format(f) for f in score.flatten()))
+        print("--> The first candidate:  ", ', '.join('{:6.2f}'.format(e) for e in score.flatten()))
         score = count_join_c2[0, :] * count_join_c2[1, :] / count_c2
-        print("--> The second candidate: ", ', '.join('{:6.2f}'.format(f) for f in score.flatten()))
+        print("--> The second candidate: ", ', '.join('{:6.2f}'.format(pp) for pp in score.flatten()))
 
         # Bigram with Smoothing
         print("Bigram with Smoothing: ")
         score = np.sum(np.log(count_join_c1 + 1), axis=0) - np.log(count_c1 + len(self.coordinate_dict))
-        print("--> The first candidate:  ", ', '.join('{:6.2f}'.format(f) for f in score.flatten()))
+        print("--> The first candidate:  ", ', '.join('{:6.2f}'.format(o) for o in score.flatten()))
         score = np.sum(np.log(count_join_c2 + 1), axis=0) - np.log(count_c2 + len(self.coordinate_dict))
-        print("--> The second candidate: ", ', '.join('{:6.2f}'.format(f) for f in score.flatten()))
-
-        # # Bigram with Smoothing
-        # print("Bigram with Smoothing: ")
-        # score = (count_join_c1[0, :] + 1) * (count_join_c1[1, :] + 1) / (np.array(count_c1) + len(self.coordinate_dict))
-        # print("--> The first candidate:  ", ', '.join('{:6.5f}'.format(f) for f in score.flatten()))
-        # score = (count_join_c2[0, :] + 1) * (count_join_c2[1, :] + 1) / (np.array(count_c2) + len(self.coordinate_dict))
-        # print("--> The second candidate: ", ', '.join('{:6.5f}'.format(f) for f in score.flatten()))
+        print("--> The second candidate: ", ', '.join('{:6.2f}'.format(l) for l in score.flatten()))
 
 
 # ################################################ #
@@ -140,13 +101,10 @@ class SentenceComplete:
 # ################################################ #
 if __name__ == '__main__':
     abs_path = os.path.abspath('.')
-    # corpus_path = os.path.join(abs_path, sys.argv[1])
-    # question_path = os.path.join(abs_path, sys.argv[2])
-
     t = time.time()
-    # process corpus
+
     print("-------- Process Corpus and Questions --------")
-    # path = '/Users/nyxfer/Documents/GitHub/nlp/sentence_completion/news-corpus-500k.txt'
+    # process corpus
     path = os.path.join(abs_path, sys.argv[1])
     with open(path, 'r') as f:
         s = f.readlines()
@@ -165,20 +123,18 @@ if __name__ == '__main__':
     corpus_list = [coordinate_dict[word] for word in corpus_list]  # int corpus
 
     # process questions
-    # path = '/Users/nyxfer/Documents/GitHub/nlp/sentence_completion/questions.txt'
     path = os.path.join(abs_path, sys.argv[2])
     with open(path, 'r') as q:
         question_list = q.readlines()
-    # answer_list = ['whether', 'through', 'piece', 'court', 'allowed',
-    #           'check', 'hear', 'cereal', 'chews', 'sell']
 
-    # train and test model
+    print()
     print("-------- Train Language Models --------")
     sc = SentenceComplete(corpus_list, coordinate_dict)
+
+    print()
     print("-------- Test Model Performance --------")
     sc.test(question_list)
+
+    print()
     print("-------- Finished --------")
     print("Total time cost: %6.2f s" % (time.time() - t))
-
-
-
